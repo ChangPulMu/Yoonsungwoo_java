ceaeabb078f43d3948d25277952dcb6664cdf6a0
ChangPulMu <16906@naver.com>
2019-02-26 14:19:45 +0900

    윤성우 Java 프로그래밍 Chapter 01 연습 문제 공부 파일

---
+++ Java_Starter_1_1_2.java
@@ -0,0 +1,12 @@
+
+public class Java_Starter_1_1_2
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("2+5=" + 2+5);
+		System.out.println("2+5=" + (2+5));
+	}
+
+}
---
+++ Java_Starter_1_1_3.java
@@ -0,0 +1,15 @@
+
+public class Java_Starter_1_1_3
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("12");
+		System.out.println(12);
+		System.out.println("1"+"2");
+		System.out.println("1"+2);
+		System.out.println(1+"2");
+	}
+
+}
